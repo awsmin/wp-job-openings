0f8605924e92e23836a1c9ea9d39a55f0213d1ea
Anantajit JG <anantajitjg2010@gmail.com>
2019-03-22 16:05:44 +0530

    ADD: More options for Recent Jobs Widget, UPDATE: Template and Hooks for Recent Jobs widget

--- inc/class-awsm-job-openings-widget.php
+++ inc/class-awsm-job-openings-widget.php
@@ -1,14 +1,20 @@
 <?php
+/**
+ * AWSM: Recent Jobs Widget
+ *
+ * @package wp-job-openings
+ * @since 1.4
+ */
 
 if( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
 class AWSM_Job_Openings_Widget extends WP_Widget {
-	public function __construct(  ) {
+	public function __construct() {
         $widget_ops = array(
 			'classname' => 'awsm_widget_recent_jobs',
-			'description' => esc_html__( 'Your site’s most recent Job listings. ', 'wp-job-openings' ),
+			'description' => esc_html__( 'Your site&#8217;s most recent Job listings.', 'wp-job-openings' ),
 			'customize_selective_refresh' => true,
 		);
         parent::__construct( 'awsm-recent-jobs', esc_html__( 'AWSM: Recent Jobs', 'wp-job-openings' ), $widget_ops );
@@ -20,49 +26,76 @@ class AWSM_Job_Openings_Widget extends WP_Widget {
 		}
 		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Jobs', 'wp-job-openings' );
 		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
-		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : get_option( 'awsm_jobs_list_per_page' );
-		$query = new WP_Query( array(
+		$default_num_jobs = get_option( 'awsm_jobs_list_per_page' );
+		$num_jobs = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : $default_num_jobs;
+		if ( ! $num_jobs ) {
+			$num_jobs = $default_num_jobs;
+		}
+		$show_spec = isset( $instance['show_spec'] ) ? $instance['show_spec'] : true;
+		$show_more = isset( $instance['show_more'] ) ? $instance['show_more'] : true;
+		
+		$query_args = apply_filters( 'awsm_widget_recent_jobs_args', array(
             'post_type'           => 'awsm_job_openings',
-			'posts_per_page'      => $number,
+			'posts_per_page'      => $num_jobs,
 			'no_found_rows'       => true,
 			'post_status'         => 'publish',
 			'ignore_sticky_posts' => true,
-		) );
+		), $instance );
+		$query = new WP_Query( $query_args );
 
 		if ( ! $query->have_posts() ) {
 			return;
 		}
+
+		if ( ! function_exists( 'get_awsm_jobs_template_path' ) ) {
+			return;
+		}
+
 		echo $args['before_widget'];
 	
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-		?>
-		<div class="awsm-job-wrap">
-			<div class="awsm-job-listings awsm-lists">
-				<?php include get_awsm_jobs_template_path( 'recent-jobs', 'widgets' ); ?>
-			</div>
-		</div>
-		<?php
+
+		do_action( 'before_awsm_recent_jobs_widget_content', $args, $instance );
+
+		include get_awsm_jobs_template_path( 'recent-jobs', 'widgets' );
+
+		do_action( 'after_awsm_recent_jobs_widget_content', $args, $instance );
+
         echo $args['after_widget'];
     }
 
     public function form( $instance ) {
-		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : get_option( 'awsm_jobs_list_per_page' );
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$num_jobs = isset( $instance['number'] ) ? $instance['number'] : get_option( 'awsm_jobs_list_per_page' );
+		$show_spec = isset( $instance['show_spec'] ) ? (bool) $instance['show_spec'] : true;
+		$show_more = isset( $instance['show_more'] ) ? (bool) $instance['show_more'] : true;
+		$title_field_id = esc_attr( $this->get_field_id( 'title' ) );
+		$num_field_id = esc_attr( $this->get_field_id( 'number' ) );
+		$spec_field_id = esc_attr( $this->get_field_id( 'show_spec' ) );
+		$more_field_id = esc_attr( $this->get_field_id( 'show_more' ) );
 		?>
-			<p><label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'wp-job-openings' ); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+			<p><label for="<?php echo $title_field_id; ?>"><?php esc_html_e( 'Title:', 'wp-job-openings' ); ?></label>
+            <input class="widefat" id="<?php echo $title_field_id; ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
 
-			<p><label for="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>"><?php esc_html_e( 'Number of jobs to show:', 'wp-job-openings' ); ?></label>
-			<input class="tiny-text" id="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" type="number" step="1" min="1" value="<?php echo esc_attr( $number ); ?>" size="3" /></p>
+			<p><label for="<?php echo $num_field_id; ?>"><?php esc_html_e( 'Number of jobs to show:', 'wp-job-openings' ); ?></label>
+			<input class="tiny-text" id="<?php echo $num_field_id; ?>" name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" type="number" step="1" min="1" value="<?php echo absint( $num_jobs ); ?>" size="3" /></p>
+
+			<p><input class="checkbox" type="checkbox"<?php checked( $show_spec ); ?> id="<?php echo $spec_field_id; ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_spec' ) ); ?>" />
+			<label for="<?php echo $spec_field_id; ?>"><?php _e( 'Display Job Specifications?' ); ?></label></p>
+
+			<p><input class="checkbox" type="checkbox"<?php checked( $show_more ); ?> id="<?php echo $more_field_id; ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_more' ) ); ?>" />
+			<label for="<?php echo $more_field_id; ?>"><?php _e( "Display 'More Details' link?" ); ?></label></p>
         <?php 
     }
 
     public function update( $new_instance, $old_instance ) {
        	$instance = $old_instance;
 		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['number'] = (int) $new_instance['number'];	
+		$instance['number'] = absint( $new_instance['number'] );
+		$instance['show_spec'] = isset( $new_instance['show_spec'] ) ? (bool) $new_instance['show_spec'] : false;
+		$instance['show_more'] = isset( $new_instance['show_more'] ) ? (bool) $new_instance['show_more'] : false;
 		return $instance;
     }
 }
--- inc/templates/widgets/recent-jobs.php
+++ inc/templates/widgets/recent-jobs.php
@@ -5,6 +5,7 @@
  * Override this by copying it to currenttheme/wp-job-openings/widgets/recent-jobs.php
  * 
  * @package wp-job-openings
+ * @since 1.4
  * @version 1.4
  */
 
@@ -14,79 +15,54 @@ if( ! defined( 'ABSPATH' ) ) {
 
 $awsm_filters = get_option( 'awsm_jobs_filter' );
 $listing_specs = get_option( 'awsm_jobs_listing_specs' );
-
-/**
- * before_awsm_jobs_listing_loop hook
- * 
- * Fires before The Loop to query for jobs
- * 
- * @since 1.1
- */
-do_action( 'before_awsm_recent_jobs_widget_listing_loop' );
-
-while( $query->have_posts() ) { $query->the_post();
-    $job_details = get_awsm_job_details();
 ?>
-<div class="awsm-list-item" id="awsm-list-item-<?php echo esc_attr__( $job_details['id'] ); ?>">
-        <div class="awsm-job-item">
-            <div class="awsm-list-left-col">
-                <?php
-                    /**
-                     * before_awsm_jobs_listing_left_col_content hook
-                     * 
-                     * @since 1.1
-                     */
-                    do_action( 'before_awsm_jobs_listing_left_col_content' );
-                ?>
 
-                <h2 class="awsm-job-post-title">
-                    <?php
-                        echo apply_filters( 'awsm_jobs_listing_title', $job_details['title'], 'list' );
-                    ?>
-                </h2>
+<div class="awsm-job-wrap">
+	<div class="awsm-job-listings awsm-lists">
+        <?php
+        do_action( 'before_awsm_recent_jobs_widget_loop', $args, $instance );
 
-                <?php
-                    /**
-                     * after_awsm_jobs_listing_left_col_content hook
-                     * 
-                     * @since 1.1
-                     */
-                    do_action( 'after_awsm_jobs_listing_left_col_content' );
-                ?>
+        while( $query->have_posts() ) :
+            $query->the_post();
+            $job_details = get_awsm_job_details();
+        ?>
+            <div class="awsm-list-item" id="awsm-list-item-<?php echo esc_attr__( $job_details['id'] ); ?>">
+                <div class="awsm-job-item">
+                    <div class="awsm-list-left-col">
+                        <?php do_action( 'before_awsm_recent_jobs_widget_left_col_content', $args, $instance ); ?>
+
+                        <h2 class="awsm-job-post-title">
+                            <?php
+                                $title = sprintf( '<a href="%2$s">%1$s</a>', $job_details['title'], $job_details['permalink'] );
+                                echo apply_filters( 'awsm_jobs_listing_title', $title, 'list' );
+                            ?>
+                        </h2>
+
+                        <?php do_action( 'after_awsm_recent_jobs_widget_left_col_content', $args, $instance ); ?>
+                    </div>
+
+                    <div class="awsm-list-right-col">
+                        <?php
+                            do_action( 'before_awsm_recent_jobs_widget_right_col_content', $args, $instance );
+
+                            if ( $show_spec ) {
+                                awsm_job_listing_spec_content( $job_details['id'], $awsm_filters, $listing_specs );
+                            }
+
+                            if ( $show_more ) {
+                                awsm_job_more_details( $job_details['permalink'], 'list' );
+                            }
+                            
+                            do_action( 'after_awsm_recent_jobs_widget_right_col_content', $args, $instance ); ?>
+                    </div>
+                </div>
             </div>
+        <?php
+        endwhile;
 
-            <div class="awsm-list-right-col">
-                <?php
-                    /**
-                     * before_awsm_jobs_listing_right_col_content hook
-                     * 
-                     * @since 1.1
-                     */
-                    do_action( 'before_awsm_jobs_listing_right_col_content' );
+        wp_reset_postdata();
 
-                    awsm_job_listing_spec_content( $job_details['id'], $awsm_filters, $listing_specs );
-
-                    awsm_job_more_details( $job_details['permalink'], 'list' );
-
-                    /**
-                     * after_awsm_jobs_listing_right_col_content hook
-                     * 
-                     * @since 1.1
-                     */
-                    do_action( 'after_awsm_jobs_listing_right_col_content' ); ?>
-            </div>
-        </div>
+        do_action( 'after_awsm_recent_jobs_widget_loop', $args, $instance );
+        ?>
     </div>
-<?php
-   }
-
-wp_reset_postdata();
-
-/**
- * after_awsm_jobs_listing_loop hook
- * 
- * Fires after The Loop
- * 
- * @since 1.1
- */
-do_action( 'after_awsm_recent_jobs_widget_listing_loop' );
+</div>
