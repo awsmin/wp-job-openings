0acedf1d458ffa5c022d899cf5b1cd32208474e9
Anantajit JG <anantajitjg2010@gmail.com>
2020-04-15 16:48:55 +0530

    ADD: Dynamic rendering for admin navigation items

--- admin/class-awsm-job-openings-info.php
+++ admin/class-awsm-job-openings-info.php
@@ -107,13 +107,13 @@ class AWSM_Job_Openings_Info {
 		return $content;
 	}
 
-	public static function is_admin_nav_page() {
+	public static function get_admin_nav_page() {
 		$is_page = false;
-		$screen = get_current_screen();
+		$screen  = get_current_screen();
 		if ( ! empty( $screen ) ) {
 			$post_type = $screen->post_type;
 			if ( ( $post_type === 'awsm_job_openings' ) || ( $post_type === 'awsm_job_application' ) ) {
-				$is_page = true;
+				$is_page = $screen->id;
 				// Check if page is the welcome page.
 				if ( isset( $_GET['page'] ) && $_GET['page'] === 'awsm-jobs-welcome-page' ) {
 					$is_page = false;
@@ -128,25 +128,79 @@ class AWSM_Job_Openings_Info {
 	}
 
 	public function admin_body_class( $classes ) {
-		if ( self::is_admin_nav_page() ) {
+		$nav_page = self::get_admin_nav_page();
+		if ( ! empty( $nav_page ) ) {
 			$classes = ' awsm-job-admin-nav-page ';
 		}
 		return $classes;
 	}
 
 	public function nav_header() {
-		if ( self::is_admin_nav_page() ) :
+		$nav_page = self::get_admin_nav_page();
+		if ( ! empty( $nav_page ) ) :
+			$nav_items = array(
+				array(
+					'id'    => 'edit-awsm_job_openings',
+					'label' => __( 'Openings', 'wp-job-openings' ),
+					'url'   => admin_url( 'edit.php?post_type=awsm_job_openings' ),
+				),
+				array(
+					'id'    => 'edit-awsm_job_application',
+					'label' => __( 'Applications', 'wp-job-openings' ),
+					'url'   => admin_url( 'edit.php?post_type=awsm_job_application' ),
+				),
+				array(
+					'id'    => 'awsm_job_openings_page_awsm-jobs-settings',
+					'label' => __( 'Settings', 'wp-job-openings' ),
+					'url'   => admin_url( 'edit.php?post_type=awsm_job_openings&page=awsm-jobs-settings' ),
+				),
+				array(
+					'id'    => 'awsm_job_openings_page_awsm-jobs-add-ons',
+					'label' => __( 'Add-Ons', 'wp-job-openings' ),
+					'url'   => admin_url( 'edit.php?post_type=awsm_job_openings&page=awsm-jobs-add-ons' ),
+				),
+				array(
+					'visible' => ! class_exists( 'AWSM_Job_Openings_Pro_Pack' ),
+					'label'   => __( 'Get PRO', 'wp-job-openings' ),
+					'url'     => 'https://1.envato.market/jjbEP',
+					'class'   => array( 'button' ),
+					'target'  => '_blank',
+				),
+			);
+			/**
+			 * Filters admin navigation items.
+			 *
+			 * @since 2.0.0
+			 *
+			 * @param array $nav_items Items data array.
+			 * @param string $nav_page The page/screen ID.
+			 */
+			$nav_items = apply_filters( 'awsm_jobs_admin_nav_items', $nav_items, $nav_page );
 			?>
 				<div class="awsm-job-admin-nav-header">
 					<div class="awsm-job-admin-nav-logo">
 						<h1><?php esc_html_e( 'WP Job Openings', 'wp-job-openings' ); ?></h1>
 					</div>
 					<ul class="awsm-job-admin-nav">
-						<li><a href="#"><?php esc_html_e( 'Openings', 'wp-job-openings' ); ?></a></li>
-						<li><a class="active" href="#"><?php esc_html_e( 'Applications', 'wp-job-openings' ); ?></a></li>
-						<li><a href="#"><?php esc_html_e( 'Settings', 'wp-job-openings' ); ?></a></li>
-						<li><a href="#"><?php esc_html_e( 'Add-Ons', 'wp-job-openings' ); ?></a></li>
-						<li><a href="#" class="button"><?php esc_html_e( 'Get PRO', 'wp-job-openings' ); ?></a></li>
+						<?php
+						foreach ( $nav_items as $nav_item ) {
+							$display = isset( $nav_item['visible'] ) ? $nav_item['visible'] : true;
+							if ( $display ) {
+								$extra_atts = '';
+								$class      = isset( $nav_item['class'] ) ? $nav_item['class'] : array();
+								if ( isset( $nav_item['id'] ) && $nav_page === $nav_item['id'] ) {
+									$class[] = 'active';
+								}
+								if ( ! empty( $class ) ) {
+									$extra_atts = ' class="' . esc_attr( implode( ' ', $class ) ) . '"';
+								}
+								if ( isset( $nav_item['target'] ) ) {
+									$extra_atts .= ' target="' . esc_attr( $nav_item['target'] ) . '"';
+								}
+								printf( '<li><a href="%2$s"%3$s>%1$s</a></li>', esc_html( $nav_item['label'] ), esc_url( $nav_item['url'] ), $extra_atts );
+							}
+						}
+						?>
 					</ul>
 				</div>
 			<?php
