d7cc63c7f5a337a5930dc1c933b6774ae12e752c
Anantajit JG <anantajitjg2010@gmail.com>
2022-11-01 18:19:19 +0530

    DEV: PHPStan based fixes

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+define( 'WP_PLUGIN_DIR', '{{wp_plugin_dir}}' );
 define( 'AWSM_JOBS_PLUGIN_DIR', '{{plugin_dir}}' );
 define( 'AWSM_JOBS_PLUGIN_URL', '{{plugin_url}}' );
 define( 'AWSM_JOBS_UPLOAD_DIR_NAME', '{{plugin_upload_dir}}' );
