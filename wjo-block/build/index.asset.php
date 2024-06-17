756bf594c26d57664492d3d85d36f16bfe1f8c7f
sruthihari <sruthi@awsm.in>
2024-06-17 11:10:53 +0530

    IMPROVED:Build changes

--- wjo-block/build/index.asset.php
+++ wjo-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0256e9f2b8a524d8250a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5c79895d1cffcde6fdf8');
