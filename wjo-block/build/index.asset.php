8cf83eb286197c9c874caf724297b17162d09e12
Aparna <aparna@awsm.in>
2024-06-20 14:48:13 +0530

    Fix : Taking build for block style check

--- wjo-block/build/index.asset.php
+++ wjo-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6b53d0987a93633eaf7b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6df1b7957483c5b3baa0');
