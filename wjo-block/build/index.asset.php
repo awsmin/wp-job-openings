79fd55f11069c10ebff250c0dc2d1f057187f9d1
sruthihari <sruthi@awsm.in>
2024-06-24 13:50:42 +0530

    ADDED:Build generated

--- wjo-block/build/index.asset.php
+++ wjo-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6bd6ca2f45e1c485a649');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6df1b7957483c5b3baa0');
