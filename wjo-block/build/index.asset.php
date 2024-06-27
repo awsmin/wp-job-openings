7700dc469449c37a5cd782b8b125c018f3b7da58
sruthihari <sruthi@awsm.in>
2024-06-27 11:40:30 +0530

    IMPROVED:Block json file

--- wjo-block/build/index.asset.php
+++ wjo-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e40b9cd640d6d633396d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '51d2673d8357d347b98a');
