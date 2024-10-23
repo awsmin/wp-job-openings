9fa585de946a34a563ea90cfc9388d099f3bf23e
nithinjohn22 <nithin@awsm.in>
2024-10-23 16:32:28 +0530

    IMPROVE: Hide expired jobs and search box option make disable by default

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '20dfb8b8106183b4da32');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '578da28cf0a1eeb580d5');
