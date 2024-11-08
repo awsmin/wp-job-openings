fc41bac7c3d2d05aeb69fba30bc0efc8eb0ace2f
Silpa TA <silpa@awsm.in>
2024-11-08 18:41:35 +0530

    Improvement: Applicant single page meta attachment fetch code improved.

--- admin/class-awsm-job-openings-meta.php
+++ admin/class-awsm-job-openings-meta.php
@@ -237,18 +237,6 @@ class AWSM_Job_Openings_Meta {
 		return $details;
 	}
 
-	// public function get_attached_file_download_url( $attachment_id, $type = 'resume', $label = '' ) {
-	// 	$query_vars = array(
-	// 		'awsm_id'     => $attachment_id,
-	// 		'awsm_nonce'  => wp_create_nonce( 'awsm_' . $type . '_download' ),
-	// 		'awsm_action' => 'download_' . $type,
-	// 	);
-	// 	if ( ! empty( $label ) ) {
-	// 		$query_vars['attachment_label'] = sanitize_title( $label );
-	// 	}
-	// 	$download_url = add_query_arg( $query_vars, get_edit_post_link() );
-	// 	return esc_url( $download_url );
-	// }
 	public function get_attached_file_download_url( $attachment_id, $type = 'resume', $label = '' ) {
 		// Check if the attachment exists in the media library
 		$file_path = get_attached_file( $attachment_id );
