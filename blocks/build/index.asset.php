3bb566d3b8226526ce83f97263c9c65329b0ed89
sruthihari <sruthi@awsm.in>
2024-12-20 16:27:53 +0530

    IMPROVED:Build Generated

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f4433a7c32a363e27ff7');
-=======
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '6db59eff4d1737b50602');
->>>>>>> a1a60794d4f88dba00a97886d8d748bc509d04de
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'fa90ff71eec57a6e4533');
