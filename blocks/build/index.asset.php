f6d31c127a646a21feb3ba804b9262f7a549c474
sruthihari <sruthi@awsm.in>
2025-04-03 18:33:11 +0530

    ADDED:Css changes to styles tab and build added

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a868f2f7453d7e1f03c9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '02d6a685c880b12b28bc');
