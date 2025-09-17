cbcf0d59a5224a7cef6acd8b968e4bec1e9e3842
sruthihari <sruthi@awsm.in>
2025-09-17 11:55:10 +0530

    IMPROVED:build generated

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd98fca1fdb5bf41cd60a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '792deda2cfd5611ae683');
