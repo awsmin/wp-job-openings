998a6d2f3f2f5f0ccc4a35519fa46c712ad84dbf
sruthihari <sruthi@awsm.in>
2025-09-23 12:45:49 +0530

    ADDED:Build generated

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '8b60d8f19e66c9bab12e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'cd2dc7e3f9ecbb301a74');
