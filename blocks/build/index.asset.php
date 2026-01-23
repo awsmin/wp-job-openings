70aa8dfbcf5f974dcaebc51a37fc5b1221a14619
Sruthi H S <sruthi@awsm.in>
2026-01-23 19:32:38 +0530

    IMPROVED: Build updated

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '34f7279d2c98c04f375e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'edb6954fc8a54211d78f');
