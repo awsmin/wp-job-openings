0c9c9f539d6e1e10bea6ce39c11bd1bf81f9f145
Sruthi H S <sruthi@awsm.in>
2026-02-04 18:55:44 +0530

    FIXED:Search refresh issue after url refresh

--- inc/templates/block-files/block-job-openings-view.php
+++ inc/templates/block-files/block-job-openings-view.php
@@ -22,73 +22,41 @@ if ( function_exists( 'get_block_wrapper_attributes' ) ) {
 } else {
 	$wrapper_attrs = 'class="' . esc_attr( $wrapper_class ) . '"';
 }
-
-if ( $query->have_posts() ) : ?>
-	<div <?php echo $wrapper_attrs; ?>>
-
-		<?php
-			/**
-			 * awsm_block_filter_form hook
-			 *
-			 * Display filter form for job listings
-			 *
-			 * @hooked AWSM_Job_Openings_Block::display_block_filter_form()
-			 *
-			 * @since 3.5.0
-			 *
-			 * @param array $attributes Attributes array from block.
-			 */
-			do_action( 'awsm_block_filter_form', $attributes );
-			do_action( 'awsm_block_form_outside', $attributes );
-		?>
-
-		<div <?php awsm_block_jobs_view_class( '', $attributes ); ?><?php awsm_block_jobs_data_attrs( array(), $attributes ); ?>>
-			<?php
-				include get_awsm_jobs_template_path( 'block-main', 'block-files' );
-			?>
-		</div>
-	</div>
+?>
+<div <?php echo $wrapper_attrs; ?>>
 	<?php
-else :
-	$filter_suffix = '_spec';
-	$job_spec      = array();
+		/**
+		 * awsm_block_filter_form hook
+		 *
+		 * Display filter form for job listings
+		 *
+		 * @hooked AWSM_Job_Openings_Block::display_block_filter_form()
+		 *
+		 * @since 3.5.0
+		 *
+		 * @param array $attributes Attributes array from block.
+		 */
+		do_action( 'awsm_block_filter_form', $attributes );
+		do_action( 'awsm_block_form_outside', $attributes );
+	?>
 
-	if ( ! empty( $_GET ) ) {
-		foreach ( $_GET as $key => $value ) {
-			if ( substr( $key, -strlen( $filter_suffix ) ) === $filter_suffix ) {
-				$job_spec[ $key ] = sanitize_text_field( $value );
-			}
-		}
-		if ( isset( $_GET['jq'] ) && $_GET['jq'] !== '' ) {
-			$search_job = sanitize_text_field( wp_unslash( $_GET['jq'] ) );
-		}
-	}
-
-	if ( ! empty( $job_spec ) || ! empty( $search_job ) ) {
-		?>
-			<div <?php echo $wrapper_attrs; ?>>
-				<?php
-					do_action( 'awsm_block_filter_form', $attributes );
-					do_action( 'awsm_filter_after_form' );
-				?>
-				<?php
-				get_block_filtered_job_terms( $attributes );
-				printf(
-					'<div class="awsm-b-jobs-pagination awsm-b-load-more-main awsm-b-no-more-jobs-container awsm-b-job-no-more-jobs-get awsm-b-jobs-none-container awsm-b-lists"><p>%s</p></div>',
-					esc_html__( 'Sorry! No jobs to show.', 'wp-job-openings' )
-				);
-				?>
-				<div <?php awsm_block_jobs_view_class( '', $attributes ); ?><?php awsm_block_jobs_data_attrs( array(), $attributes ); ?>>
-					<?php include get_awsm_jobs_template_path( 'block-main', 'block-files' ); ?>
-				</div>
+	<div <?php awsm_block_jobs_view_class( '', $attributes ); ?><?php awsm_block_jobs_data_attrs( array(), $attributes ); ?>>
+		<?php if ( $query->have_posts() ) : ?>
+			<?php include get_awsm_jobs_template_path( 'block-main', 'block-files' ); ?>
+		<?php else : ?>
+			<div class="awsm-jobs-none-container">
+				<p><?php awsm_no_jobs_msg(); ?></p>
 			</div>
-		<?php
-	} else {
-		?>
-		<div class="awsm-b-jobs-none-container">
-			<p><?php awsm_no_jobs_msg(); ?></p>
-		</div>
-		<?php
-	}
-endif;
+		<?php endif; ?>
+	</div>
+</div>
+<?php
+/**
+ * Fires after the job listing content.
+ *
+ * @param array $attributes Attributes array from block.
+ */
+do_action( 'after_awsm_block_jobs_listing', $attributes );
+?>
+
 
