144a0dc56f058af4e7f7d8bf5960b26e498cb9c8
Sruthi H S <sruthi@awsm.in>
2026-02-19 14:44:58 +0530

    FIXED:Label issues in inspector controls

--- blocks/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '56dee100d0fe5c93fef9');
