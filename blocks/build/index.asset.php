4f25da8d0961e0cca305891f96286c181f540014
Sruthi H S <sruthi@awsm.in>
2026-02-20 13:27:44 +0530

    IMPROVED:layout default changed

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '6444378bcd6d8d7c9d64');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'aa5f4df656da6c18816a');
