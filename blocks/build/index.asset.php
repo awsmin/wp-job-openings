73fcac8faf5b16bf7725a5cd12146c08ea49adf4
Sruthi H S <sruthi@awsm.in>
2026-02-27 16:18:50 +0530

    IMPROVED:Label font

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f3a93ef79d988a616477');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'fb4652df05cc608d432f');
