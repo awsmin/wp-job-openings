92e35846544279560b7ad77fbcecad3242c987c8
Sruthi H S <sruthi@awsm.in>
2026-03-05 19:37:42 +0530

    ADDED:Block generated files

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '5e11bb725a80f577ecf8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '1d396826908539a671a4');
