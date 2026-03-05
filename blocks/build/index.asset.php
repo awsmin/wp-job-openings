93dcddf8e83d6f5a99c3b57f2e3f7804e1498fb1
Sruthi H S <sruthi@awsm.in>
2026-03-05 16:18:11 +0530

    REMOVED:Color for label post title

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '201b43a36f4b93a0a1af');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '5e11bb725a80f577ecf8');
