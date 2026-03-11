66d5ce6c2837487123bfd0af57ab84813e282d24
Sruthi H S <sruthi@awsm.in>
2026-03-11 13:12:35 +0530

    FIXED:Jobs without specs will show now onwards

--- inc/class-awsm-job-openings-block.php
+++ inc/class-awsm-job-openings-block.php
@@ -517,7 +517,7 @@ class AWSM_Job_Openings_Block {
 	public static function get_block_spec_terms( $spec ) {
 		$terms_args = array(
 			'taxonomy'   => $spec,
-			'hide_empty' => true,
+			'hide_empty' => false,
 		);
 		$terms      = get_terms( $terms_args );
 		if ( is_wp_error( $terms ) ) {
