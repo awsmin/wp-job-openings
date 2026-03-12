0e78fcbca6c16b133a1d485de7c78f1452c3ceaa
Sruthi H S <sruthi@awsm.in>
2026-03-12 12:06:03 +0530

    ADDED:Build generated

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '630cd62af8f7786879ec');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '19700da784768accf49a');
