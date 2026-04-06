6688b2adb899fc69ef659cdb1b463e3d2c28f13a
Sruthi H S <sruthi@awsm.in>
2026-04-06 14:11:46 +0530

    REMOVED:Beta from title block

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'db33c9b56e174a7e4777');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '1a5b86563ea0105539cd');
