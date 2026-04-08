80297cec4ed3a607f5b285bec29e744ca51bcc51
Silpa TA <silpa@awsm.in>
2026-04-08 10:05:42 +0530

    Change: Name changed WPJO to hirezoot

--- admin/class-awsm-job-openings-meta.php
+++ admin/class-awsm-job-openings-meta.php
@@ -62,7 +62,7 @@ class AWSM_Job_Openings_Meta {
 
 		if ( ! class_exists( 'AWSM_Job_Openings_Pro_Pack' ) ) {
 			add_meta_box( 'awsm-application-actions-meta', esc_html__( 'Actions', 'wp-job-openings' ), array( $this, 'application_actions_meta_handler' ), 'awsm_job_application', 'side', 'high' );
-			add_meta_box( 'awsm-get-the-pro-pack-meta', esc_html__( 'Upgrade to WPJO Pro', 'wp-job-openings' ), array( $this, 'get_pro_meta_handler' ), 'awsm_job_application', 'side', 'low' );
+			add_meta_box( 'awsm-get-the-pro-pack-meta', esc_html__( 'Upgrade to Hirezoot Pro', 'wp-job-openings' ), array( $this, 'get_pro_meta_handler' ), 'awsm_job_application', 'side', 'low' );
 		}
 	}
 
