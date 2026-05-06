0e7f685965e1dffab3e89f3a4c22581eff0ad92b
Sruthi H S <sruthi@awsm.in>
2026-05-06 14:52:35 +0530

    IMPROVED:Title for block changed to Job Listings(Beta)

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '6a02278133dc77ad68ee');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd6e3057c825b6cb30884');
