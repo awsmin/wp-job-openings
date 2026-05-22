fb18a4544ee83abccbdc6d6252c83431390c3945
Sruthi H S <sruthi@awsm.in>
2026-05-22 09:14:57 +0530

    Revert "FIXED: export WidgetInspectorControls as default to prevent "Error loading block:" on re-render"
    This reverts commit d22d685023837422f11d481e260ef983de193520.

---
+++ blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '0b71967a375989382da1');
