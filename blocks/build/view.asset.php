35678c76b869d4ed9d10c48d6ef4b798db00c498
Sruthi H S <sruthi@awsm.in>
2026-06-18 17:42:25 +0530

    IMPROVED:pagination repeat issue

--- blocks/build/view.asset.php
+++ blocks/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '0a585c64b051d8d09d27');
+<?php return array('dependencies' => array('jquery'), 'version' => 'd5d9da86ea96f34d4cba');
