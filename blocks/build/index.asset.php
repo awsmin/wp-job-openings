d2a6bb71cf71fd02153d1e674bbce72248ea28fa
Sruthi H S <sruthi@awsm.in>
2026-07-21 10:57:51 +0530

    FIXED:HR-554 , hovering the cursor

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => '6768af3286dcbc64f8ec');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd088f691c1fe0b3bdb61');
